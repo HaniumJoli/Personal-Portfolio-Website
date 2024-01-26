b604ae077190dc65985d631b2b0524477a4971c4
HaniumJoli <haniumjoli1015@gmail.com>
2024-01-27 00:56:44 +0600

    added a basic javascript code and made some fixes

---
+++ Script/index-script.js
@@ -0,0 +1,7 @@
+let typed=new Typed('.typed',{
+    strings:["a Web Developer","a Programmer","a Designer","an Artist"],
+    typeSpeed:75,
+    backSpeed:75,
+    backDelay:1000,
+    loop:true
+});
