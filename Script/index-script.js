7dd0a6d6371471fb3e5da40d335cc7ec586b06a6
HaniumJoli <haniumjoli1015@gmail.com>
2024-02-25 07:31:57 +0600

    .

--- Script/index-script.js
+++ Script/index-script.js
@@ -5,3 +5,35 @@ let typed=new Typed('.typed',{
     backDelay:1000,
     loop:true
 });
+
+showside = document.getElementById('showside');
+sidebar = document.querySelector('.sidebar');
+showside.addEventListener('click', () => {
+    openSideBar();
+});
+
+hideside = document.getElementById('hideSide');
+hideside.addEventListener('click', () => {
+    closeSideBar();
+    
+});
+
+document.addEventListener('click', (e) => {
+    if (!sidebar.contains(e.target) && !showside.contains(e.target)) {
+        closeSideBar();
+    }
+});
+
+function openSideBar() {
+    sidebar.classList.remove('close-sidebar');
+    sidebar.classList.add('open-sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closeSideBar() {
+    sidebar.classList.remove('open-sidebar');
+    sidebar.classList.add('close-sidebar');
+    setTimeout(() => {
+        sidebar.style.display = 'none';
+    }, 500);
+}
